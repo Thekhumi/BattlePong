8fac70e63c61429f1f71a09a80af99b90fbd80cf
Gaspar Nuñez <gaspacho09@hotmail.com>
2018-08-29 14:10:25 -0300

    Basic Pong Demo

---
+++ BattlePong/Assets/Ball.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+	[SerializeField] float speed = 5f;
+	// Use this for initialization
+	void Start () {
+		float sx = Random.Range (0, 2) == 0 ? -1 : 1;
+		float sy = Random.Range (0, 2) == 0 ? -1 : 1;
+
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (speed * sx, speed * sy);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ BattlePong/Assets/Bumper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bumper : MonoBehaviour {
+
+	[SerializeField]bool isBump1;
+	[SerializeField]float speed = 5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isBump1) {
+			transform.Translate (0.0f, Input.GetAxis ("Vertical") * speed * Time.deltaTime, 0.0f);
+		} else {
+			transform.Translate (0.0f, Input.GetAxis ("Vertical2") * speed * Time.deltaTime, 0.0f);
+		}
+	}
+}
