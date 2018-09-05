f82a13a462781f45cc4f46d4f617940402bcf06c
Leo Perugini <xweetyk@gmail.com>
2018-09-05 04:44:05 -0300

    GameManager
    -game manager updated.
    -camera and game elements movement between screens.
    -still no win/lose condition

--- BattlePong/Assets/Ball.cs
+++ BattlePong/Assets/Ball.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Ball : MonoBehaviour {
 	[SerializeField] float speed = 5f;
 	[SerializeField] float sum = 0f;
+	[SerializeField] GameManager _manager;
+	private float _originalSpeed;
+
 	Rigidbody2D body;
 	// Use this for initialization
 	void Start () {
@@ -12,17 +15,40 @@ public class Ball : MonoBehaviour {
 		float sy = Random.Range (0, 2) == 0 ? -1 : 1;
 		body = GetComponent<Rigidbody2D>();
 		body.velocity = new Vector2 (speed * sx, speed * sy);
+		_originalSpeed = speed;
 	}
 
 	void OnCollisionEnter2D(Collision2D otro){
-		Debug.Log (otro.gameObject.name);	
-		if (otro.gameObject.layer == 8) {
-			speed += sum;
-			Debug.Log ("bump");
-		}
+		//Debug.Log (otro.gameObject.name);
+		//if (otro.gameObject.tag != "END GAME") {
+			switch (otro.gameObject.layer) {
+			case 8:
+				speed += sum;
+			//Debug.Log ("bump");
+				break;
+			case 9:
+				_manager.SetWinnerRight ();
+				break;
+			case 10:
+				_manager.SetWinnerLeft ();
+				break;
+			}
+	//	}
 	}
 
 	void LateUpdate () {
 		body.velocity = speed * (body.velocity.normalized);
+	}
+	public void Reset(){
+		speed = _originalSpeed;
+		float sx = Random.Range (0, 2) == 0 ? -1 : 1;
+		float sy = Random.Range (0, 2) == 0 ? -1 : 1;
+		body.velocity = new Vector2 (speed * sx, speed * sy);
+	}
+	public void Stop(){
+		speed = 0;
+	}
+	public void ResetPosition(){
+		transform.position = transform.parent.position;
 	}
 }
---
+++ BattlePong/Assets/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	private bool _winnerLeft = false;
+	private bool _winnerRight = false;
+	[Tooltip("From Left to Right")]
+	[SerializeField] GameObject[] _cameraScreens;
+	[Tooltip("From Left to Right")]
+	[SerializeField] GameObject[] _levelScreens;
+	[SerializeField] GameObject _levelElements;
+	[SerializeField] GameObject _ball;
+	[SerializeField] Camera _mainCamera;
+	[SerializeField] private float cameraSpeed;
+	private int _cameraState;
+	void Start(){
+		_cameraState = _cameraScreens.Length / 2;
+	}
+	void Update(){
+		if (_winnerLeft) {
+			CameraMov ();
+			BallStart ();
+			_winnerRight = false; _winnerLeft = false;
+		}
+		if (_winnerRight) {
+			CameraMov ();
+			BallStart ();
+			_winnerRight = false; _winnerLeft = false;
+		}
+	}
+	public void SetWinnerLeft(){
+		_winnerLeft = true;
+		if (_cameraState != 0) {
+			_cameraState -= 1;
+		}
+	}
+	public void SetWinnerRight(){
+		_winnerRight = true;
+		if (_cameraState != (_cameraScreens.Length - 1)) {
+			_cameraState += 1;
+		}
+	}
+	private void BallReset(){
+		_ball.GetComponent<Ball> ().Reset ();
+	}
+	private void BallStart(){
+		_ball.GetComponent<Ball> ().Stop ();
+		_ball.GetComponent<Ball> ().ResetPosition ();
+		Invoke ("BallReset", 0.5f);
+	}
+	private void CameraMov(){
+		_mainCamera.transform.position = _cameraScreens [_cameraState].transform.position;
+		_levelElements.transform.position = _levelScreens [_cameraState].transform.position;
+	}
+}
