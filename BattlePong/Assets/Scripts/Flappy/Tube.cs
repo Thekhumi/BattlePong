82a5945650e4d61d5b4874971a1d7a0e137a4f40
Gaspar Nuñez <gaspacho09@hotmail.com>
2018-09-24 02:22:27 -0300

    Flappy walls

---
+++ BattlePong/Assets/Scripts/Flappy/Tube.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tube : MonoBehaviour {
+	float _startTime;
+	[SerializeField] bool _isRising = false;
+	[SerializeField] float _switchTime = 3f;
+	[SerializeField] float speed = 1;
+	float timer;
+	// Use this for initialization
+	void Start () {
+		_startTime = Random.Range (1f, 3f);
+		Invoke ("Move", _startTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_isRising) {
+			transform.Translate (0, speed * Time.deltaTime, 0);
+		} else {
+			transform.Translate (0, -speed * Time.deltaTime, 0);
+		}
+	}
+
+	void Move(){
+		_isRising = !_isRising;
+		Invoke ("Move", _switchTime);
+	}
+}
