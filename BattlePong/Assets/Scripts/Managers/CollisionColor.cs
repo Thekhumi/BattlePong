3c184ad8e75b10d9f3448e28641d7854f916c4bf
Leo Perugini <xweetyk@gmail.com>
2018-10-14 20:03:32 -0300

    Pinball Update

--- BattlePong/Assets/Scripts/Managers/CollisionColor.cs
+++ BattlePong/Assets/Scripts/Managers/CollisionColor.cs
@@ -7,12 +7,19 @@ public class CollisionColor : MonoBehaviour {
 	[SerializeField] private Sprite _normal;
 	[SerializeField] private Sprite _light;
 	[SerializeField] private float _lightDelay=0.1f;
+	[SerializeField] private bool _isPinball;
 	private SpriteRenderer _renderer;
 	void Start () {
 		_renderer = gameObject.GetComponent<SpriteRenderer>();
+		if (_isPinball) {
+			gameObject.GetComponentInChildren<ParticleSystem> ().Stop ();
+		}
 	}
 	void OnCollisionEnter2D (Collision2D otro){
 		_renderer.sprite = _light;
+		if (_isPinball) {
+			gameObject.GetComponentInChildren<ParticleSystem> ().Play ();
+		}
 		Invoke ("ChangeBack", _lightDelay);
 	}
 	//void OnCollisionExit2D (Collision2D otro){
