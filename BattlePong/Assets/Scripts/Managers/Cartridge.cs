2f1fa9ac11f9514c68c79bb70e6ef12bdf8a541c
Leo Perugini <xweetyk@gmail.com>
2018-10-18 18:08:40 -0300

    Menu Update
    game over yeeeeeaaahh

--- BattlePong/Assets/Scripts/Managers/Cartridge.cs
+++ BattlePong/Assets/Scripts/Managers/Cartridge.cs
@@ -4,37 +4,41 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Cartridge : MonoBehaviour 
-, IPointerClickHandler
-, IPointerEnterHandler
-, IPointerExitHandler{
+public class Cartridge : MonoBehaviour {
+	[SerializeField] SpriteRenderer[] _cartuchos;
+	[SerializeField] Sprite[] _spriteLight;
+	[SerializeField] Sprite[] _spriteOff;
+	[SerializeField] SpriteRenderer _text;
+	[SerializeField] Sprite[] _textSprites;
+	[SerializeField] SceneChange _scene;
+	private int _cont;
 
-	[SerializeField] Sprite _cartridgeSprite;
-	[SerializeField] MainMenuManager _cManager;
-	[SerializeField] [Range(1,5)] int _buttonNum;
-	Image _button;
-	private bool _selected;
-
-	void Awake () {
-		_button = GetComponent<Image> ();
-		_selected = false;
+	void Start(){
+		_cont = 0;
 	}
 	void Update(){
-		if (_selected) {
-			_selected=_cManager.MovingCartridge (_cartridgeSprite, _selected,_buttonNum);
+		if (Input.GetButtonDown ("Left")||Input.GetButtonDown ("Down")) {
+			if (_cont != 0) {
+				_cont--;
+			} else {_cont = 4;}
 		}
-	}
-	public void OnPointerClick(PointerEventData eventData)
-	{
-		_cManager.Clicked ();
-	}
-	public void OnPointerEnter(PointerEventData eventData)
-	{
-		_button.color = Color.yellow;
-		_selected = true;
-	}
-	public void OnPointerExit(PointerEventData eventData)
-	{
-		_button.color = Color.white;
+		if (Input.GetButtonDown ("Right")||Input.GetButtonDown ("Up")) {
+			if (_cont != 4) {
+				_cont++;
+			} else {_cont = 0;}
+		}
+		if (Input.GetButtonDown ("Submit")) {
+			_scene.LoadScene (_cont+2);
+		}
+		for (int i = 0; i < _cartuchos.Length; i++) {
+			if (i == _cont) {
+				_cartuchos [i].sprite = _spriteLight [i];
+			} else {
+				_cartuchos [i].sprite = _spriteOff [i];
+			}
+		}
+
+		Debug.Log (_textSprites.Length);
+		_text.sprite = _textSprites [_cont];
 	}
 }
--- BattlePong/Assets/Scripts/Managers/MainMenuManager.cs
+++ BattlePong/Assets/Scripts/Managers/MainMenuManager.cs
@@ -5,55 +5,14 @@ using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour {
 
-	[SerializeField] Sprite _base;
-	[SerializeField] GameObject _cartridge;
-	[SerializeField] Transform _target;
-	[SerializeField] float _speed;
-	int _cont;
-	int _selected;
-	int _lastSelected;
-	Image _button;
-	Vector3 _origPos;
-	Vector3 _inserted;
-
-	void Awake () {
-		_button = GetComponent<Image> ();
-	}
+	[SerializeField] Text _version;
+	[SerializeField] SceneChange _scene;
 	void Start(){
-		_origPos = _cartridge.transform.position;
-		_cont = 0;
-		_selected = _lastSelected = 0;
-		_inserted = _cartridge.transform.position - (new Vector3 (0.0f, 0.9f, 0.0f));
+		_version.text = "V " + Application.version;
 	}
-
-	public bool MovingCartridge(Sprite cartSprite, bool selected, int select){
-		_selected = select;
-		if(_lastSelected!=_selected)
-		switch (_cont) {
-			case 0:
-				_lastSelected = select;
-				_cartridge.transform.position = Vector3.MoveTowards (_cartridge.transform.position, _target.position, _speed * Time.deltaTime);
-				if (_cartridge.transform.position == _target.position) {
-					if (cartSprite != null) {
-						_cartridge.GetComponent<SpriteRenderer> ().sprite = cartSprite;
-					} else {
-						_cartridge.GetComponent<SpriteRenderer> ().sprite = _base;
-					}
-					_cont++;
-				}
-				return true;
-			break;
-		case 1:
-			_cartridge.transform.position = Vector3.MoveTowards (_cartridge.transform.position, _origPos, _speed * Time.deltaTime);
-			if (_cartridge.transform.position==_origPos) {
-				_cont = 0;
-				return false;
-			}
-			break;
+	void Update(){
+		if (Input.GetButtonDown ("Submit")) {
+			_scene.LoadScene (1);
 		}
-		return true;
-	}
-	public void Clicked(){
-		_cartridge.transform.position = Vector3.MoveTowards (_cartridge.transform.position, _inserted , _speed * Time.deltaTime);
 	}
 }
