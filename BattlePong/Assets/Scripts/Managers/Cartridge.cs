cb11a6cf9aa9c194a0bada067fc3d980e8d6f6b2
Leo Perugini <xweetyk@gmail.com>
2018-10-25 16:39:08 -0300

    Menu Restored

--- BattlePong/Assets/Scripts/Managers/Cartridge.cs
+++ BattlePong/Assets/Scripts/Managers/Cartridge.cs
@@ -5,16 +5,39 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class Cartridge : MonoBehaviour {
-	[SerializeField] SpriteRenderer[] _cartuchos;
+	[SerializeField] GameObject[] _cartuchos;
 	[SerializeField] Sprite[] _spriteLight;
 	[SerializeField] Sprite[] _spriteOff;
-	[SerializeField] SpriteRenderer _text;
 	[SerializeField] Sprite[] _textSprites;
+	[SerializeField] Transform [] _target;
+	[SerializeField] Transform _mainTarget;
+	[SerializeField] Transform _insertedTarget;
+	[SerializeField] Transform _aBitUp;
+	[SerializeField] SpriteRenderer _text;
 	[SerializeField] SceneChange _scene;
+	[SerializeField] ParticleSystem[] _particles;
+	[SerializeField] float _vel;
+	[SerializeField] float _waitTime;
+	private SpriteRenderer[] _cartSprite;
 	private int _cont;
+	private int _activated;
+	private bool _press;
+	private bool _checked;
 
+	void Awake(){
+		_cartSprite = new SpriteRenderer[_cartuchos.Length];
+		for (int i = 0; i < _cartuchos.Length; i++) {
+			_cartSprite [i] = _cartuchos [i].GetComponent<SpriteRenderer> ();
+		}
+		foreach (var part in _particles) {
+			part.Stop ();
+		}
+	}
 	void Start(){
 		_cont = 0;
+		_activated = 0;
+		_press = false;
+		_checked = false;
 	}
 	void Update(){
 		if (Input.GetButtonDown ("Left")||Input.GetButtonDown ("Down")) {
@@ -28,17 +51,65 @@ public class Cartridge : MonoBehaviour {
 			} else {_cont = 0;}
 		}
 		if (Input.GetButtonDown ("Submit")) {
-			_scene.LoadScene (_cont+2);
+			_press = true;
+		}
+		if (_press) {
+			switch (_activated) {
+			case 0:
+				for (int i = 0; i < _cartuchos.Length; i++) {
+					if (i == _cont) {
+						_cartuchos [i].transform.position = Vector3.MoveTowards (_cartuchos [i].transform.position, _mainTarget.position, _vel/2);
+					} else {
+						_cartuchos [i].transform.position = Vector3.MoveTowards (_cartuchos [i].transform.position, _target [i].position, _vel);
+						if (_cartuchos [_cont].transform.position == _mainTarget.position) {
+							Invoke ("Wait", _waitTime);
+						}
+					}
+				}
+				break;
+			case 1:
+				_cartuchos [_cont].transform.position = Vector3.MoveTowards (_cartuchos [_cont].transform.position, _aBitUp.position, _vel / 3);
+				if (_cartuchos [_cont].transform.position == _aBitUp.position) {
+					_activated = 2;
+				}
+				break;
+			case 2:
+				_cartuchos [_cont].transform.position = Vector3.MoveTowards (_cartuchos [_cont].transform.position, _insertedTarget.position, _vel / 3);
+				if (_cartuchos [_cont].transform.position == _insertedTarget.position) {
+					_checked = false;
+					_activated = 3;
+				}
+				break;
+			case 3:
+				Particles ();
+				break;
+			}
 		}
 		for (int i = 0; i < _cartuchos.Length; i++) {
 			if (i == _cont) {
-				_cartuchos [i].sprite = _spriteLight [i];
+				_cartSprite [i].sprite = _spriteLight [i];
 			} else {
-				_cartuchos [i].sprite = _spriteOff [i];
+				_cartSprite [i].sprite = _spriteOff [i];
 			}
 		}
-
-		Debug.Log (_textSprites.Length);
 		_text.sprite = _textSprites [_cont];
+	}
+	private void Wait(){
+		if (!_checked) {
+			_activated = 1;
+			_checked = true;
+		}
+	}
+	private void Particles(){
+		if (!_checked) {
+			foreach (var part in _particles) {
+				part.Play ();
+			}
+			Invoke ("Scene", 0.5f);
+			_checked=true;
+		}
+	}
+	private void Scene(){
+			_scene.LoadScene (_cont+2);
 	}
 }
