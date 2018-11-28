a75e65a89be8504cac51ef9bb5bc17b120cc2859
Gaspar Nuñez <gaspacho09@hotmail.com>
2018-11-27 23:22:32 -0300

    Pinball AI
    Needs balancing, machines will destroy us.

---
+++ BattlePong/Assets/Scripts/Pinball/RivalAIPinball.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RivalAIPinball : MonoBehaviour {
+	[SerializeField] float speed = 50f;
+	Rigidbody2D _Flipper;
+	void Start(){
+		_Flipper = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	void OnTriggerStay2D(Collider2D otro){
+		if (otro.gameObject.tag == "PinballBall") {
+			_Flipper.AddTorque (speed,ForceMode2D.Impulse);
+		}
+	}
+}
