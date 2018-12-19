0c0f5761fc748aaf472699ce0cc661df4c2e8c4a
Gaspar Nuñez <gaspacho09@hotmail.com>
2018-12-19 02:12:29 -0300

    FixResolution values and update

--- BattlePong/Assets/Scripts/Managers/SettingsManager.cs
+++ BattlePong/Assets/Scripts/Managers/SettingsManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,42 +11,52 @@ public class SettingsManager : MonoBehaviour {
 	[SerializeField] Dropdown _resolutionDropDown;	
 	[SerializeField] AudioClip _testSound;
 	private Resolution[] _resolutions;
-	private bool _testOn;
+	private bool _turnOnSliders;
 
-	void Start(){
+	void Awake(){
 		Cursor.lockState = CursorLockMode.Locked;
 		Cursor.visible = false;
 		_musicSlider.value = MusicManager.Instance.musicVolume;
 		_sfxSlider.value = MusicManager.Instance.sfxVolume;
 		_isFullscreen.isOn = Screen.fullScreen;
-		_resolutions = Screen.resolutions;
+		updateResolutions ();
+		_turnOnSliders = true;
+	}
+	public void updateResolutions(){
+		_resolutions = Screen.resolutions.Select (resolution => new Resolution {
+			width = resolution.width,
+			height = resolution.height
+		}).Distinct ().ToArray ();
 		_resolutionDropDown.ClearOptions ();
 		List<string> resOptions = new List<string> ();
 		int currentRes = 0;
 		for (int i = 0; i < _resolutions.Length; i++) {
 			string option = _resolutions[i].width + " x " + _resolutions[i].height;
-			resOptions.Add (option);
-			if (_resolutions [i].width == Screen.currentResolution.width
-			   && _resolutions [i].height == Screen.currentResolution.height) {
+			if (_resolutions [i].width == Screen.width
+			    && _resolutions [i].height == Screen.height) {
 				currentRes = i;
 			}
+			resOptions.Add (option);
 		}
+
 		_resolutionDropDown.AddOptions (resOptions);
 		_resolutionDropDown.value = currentRes;
+		Debug.Log (Screen.currentResolution.width + " " + Screen.currentResolution.height);
 		_resolutionDropDown.RefreshShownValue ();
-		_testOn = true;
 	}
 
 	public void setResolution(int resolution){
-		Resolution res = _resolutions [resolution];
-		Screen.SetResolution (res.width, res.height,Screen.fullScreen);
+		if (_turnOnSliders) {
+			Resolution res = _resolutions [resolution];
+			Screen.SetResolution (res.width, res.height, Screen.fullScreen);
+		}
 	}
 
 	public void setSfxVolume(float volume){
 		MusicManager.Instance.sfxVolume = volume;
 		MusicManager.Instance.updateVolume ();
 		PlayerPrefs.SetFloat ("pSfxVolume", volume);
-		if (_testOn) {
+		if (_turnOnSliders) {
 			testSound ();
 		}
 	}
